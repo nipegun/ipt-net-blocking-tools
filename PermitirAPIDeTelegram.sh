46125571ec6c9772c10393ea002fb6f40d7c7acd
NiPeGun <nipegun@gmail.com>
2019-06-21 05:03:38 +0200

    Update PermitirAPIDeTelegram.sh

--- PermitirAPIDeTelegram.sh
+++ PermitirAPIDeTelegram.sh
@@ -4,6 +4,6 @@ IPAPITelegram=$(ping -c 1 api.telegram.org | awk -F'[()]' '/PING/{print $2}')
 echo ""
 echo "Permitiendo tráfico desde y hacia la API de Telegram ($IPAPITelegram)..."
 echo ""
-iptables -A INPUT -p tcp -s $IPAPITelegram -j ACCEPT
-iptables -A OUTPUT -p tcp -d  $IPAPITelegram -j ACCEPT
+iptables -I INPUT -p tcp -s $IPAPITelegram -j ACCEPT
+iptables -I OUTPUT -p tcp -d  $IPAPITelegram -j ACCEPT
 
