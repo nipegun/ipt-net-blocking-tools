0615ab0185c2275fa2425ee8cd6d8f8a461081c6
NiPeGun <nipegun@gmail.com>
2019-06-21 05:34:45 +0200

    Update PermitirAPIDeTelegram.sh

--- PermitirAPIDeTelegram.sh
+++ PermitirAPIDeTelegram.sh
@@ -7,5 +7,8 @@
 #iptables -I INPUT -p tcp -s $IPAPITelegram -j ACCEPT
 #iptables -I OUTPUT -p tcp -d  $IPAPITelegram -j ACCEPT
 
+echo ""
+echo "Anulando la línea que contiene la IP de api.telegram.org de la lista de IPs de Rusia..."
+echo ""
 sed -i -e's|149.154.160.0/20|#149.154.160.0/20|g' /root/non-spanish-country-blocker/ru.txt
 
