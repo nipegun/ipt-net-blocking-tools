c3fe2f0b4db127a6c345bd147c0d9174ea78fac6
NiPeGun <nipegun@gmail.com>
2019-10-28 01:21:33 +0100

    Update NFTables-Configurar.sh

--- NFTables-Configurar.sh
+++ NFTables-Configurar.sh
@@ -463,13 +463,11 @@ echo "    type ipv4_addr" >>                                                  /e
 echo "    flags interval" >>                                                  /etc/nftables.conf
 echo "    elements = { \$CN.ipv4, \$RU.ipv4, \$UA.ipv4, \$VN.ipv4 }" >>       /etc/nftables.conf
 echo "  }" >>                                                                 /etc/nftables.conf
-echo "" >>                                                                    /etc/nftables.conf
 echo "  set country-ipv6-blacklist {" >>                                      /etc/nftables.conf
 echo "    type ipv6_addr" >>                                                  /etc/nftables.conf
 echo "    flags interval" >>                                                  /etc/nftables.conf
 echo "    elements = { \$CN.ipv6, \$RU.ipv6, \$UA.ipv6, \$VN.ipv6 }" >>       /etc/nftables.conf
 echo "  }" >>                                                                 /etc/nftables.conf
-echo "" >>                                                                    /etc/nftables.conf
 echo "  chain ingress {" >>                                                   /etc/nftables.conf
 echo "    type filter hook ingress device eth0 priority 0; policy accept;" >> /etc/nftables.conf
 echo "    ip saddr @country-ipv4-blacklist counter drop" >>                   /etc/nftables.conf
