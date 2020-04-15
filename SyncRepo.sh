d0068adb30905cd56d79a97386000f105d2eca3b
NiPeGun <nipegun@gmail.com>
2020-04-15 04:50:08 +0200

    Update SyncRepo.sh

--- SyncRepo.sh
+++ SyncRepo.sh
@@ -19,11 +19,11 @@ wget -q --tries=10 --timeout=20 --spider https://github.com
     echo ""
     echo -e "  ${ColorGreen}Syncing ipt-net-blocking-tools repo...${EndColor}" 
     echo ""
-    rm /root/scripts/ipt-net-blocking-tools -R
+    rm /root/scripts/ipt-net-blocking-tools -R 2> /dev/null
     cd /root/scripts
     git clone --depth=1 https://github.com/nipegun/ipt-net-blocking-tools
-      rm /root/scripts/ipt-net-blocking-tools/.git -R
-      rm /root/scripts/ipt-net-blocking-tools/README.md
+      rm /root/scripts/ipt-net-blocking-tools/.git -R 2> /dev/null
+      rm /root/scripts/ipt-net-blocking-tools/README.md 2> /dev/null
     find /root/scripts/ipt-net-blocking-tools/ -type f -iname "*.sh" -exec chmod +x {} \;
     echo ""
     echo -e "${ColorGreen}Repo synced correctly.${EndColor}"
