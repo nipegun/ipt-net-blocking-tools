c2a6165e1b40a6846016cb23b7d53dfe18b0d350
NiPeGun <nipegun@gmail.com>
2021-02-18 05:42:09 +0100

    Update NSSC-IPTables-Block.sh

--- non-spanish-speaking-countries/NSSC-IPTables-Block.sh
+++ non-spanish-speaking-countries/NSSC-IPTables-Block.sh
@@ -18,7 +18,7 @@ ColorRojo='\033[1;31m'
 ColorVerde='\033[1;32m'
 FinColor='\033[0m'
 
-Archivo=/root/scripts/non-spanish-country-blocker/$1.txt
+Archivo=/root/scripts/non-spanish-speaking-countries/$1.txt
 
 if [ $# -ne $CantArgsEsperados ]
   then
